007a5e662e87f17eec5698bc369473201a7d0457
moh1med <mohamedch047@gmail.com>
2023-07-27 15:02:05 +0100

    Assinmenet Completed

--- index.js
+++ index.js
@@ -1,11 +1,38 @@
-function scuberGreetingForFeet(){
+function scuberGreetingForFeet(ride){
   // Write your code here!
+  let result;
+  if (ride <= 400){
+    result = 'This one is on me!';
+
+  }
+  else if ( ride > 400 && ride < 2000  ){
+    result = 'That will be twenty bucks.'
+  }
+  else if (ride <= 2500){
+    result = 'I will gladly take your thirty bucks.'
+  }
+  else[
+    result = 'No can do.'
+  ]
+  return result;
 }
 
-function ternaryCheckCity(){
+function ternaryCheckCity(destination){
   // Write your code here!
+  let response = destination === "NYC"
+  ? "Ok, sounds good." : "No go.";
+  return response;
+  
 }
 
-function switchOnCharmFromTip(){
+function switchOnCharmFromTip(tip){
   // Write your code here!
+  switch (tip) {
+    case 'generous':
+      return "Thank you so much.";
+    case 'not as generous':
+      return "Thank you.";
+    default:
+      return "Bye.";
+  }
 }
